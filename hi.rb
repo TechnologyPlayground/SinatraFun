65e89d1a4190a425415f88c349efd2897813e4a7
William Benz <william.a.benz@gmail.com>
2012-09-13 21:11:57 -0500

    Added some extra testing methods

--- hi.rb
+++ hi.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require 'sinatra/content_for'
 require 'Haml'
+require 'json'
+
+configure do
+  set :title, "you smell"
+end
 
 get '/hi' do
   "Hello World!"
@@ -21,4 +27,15 @@ end
 post '/yourcar' do
   @bar = params[:car]
   haml :foo
+end
+
+get '/test.:type' do
+  @hashy = { :id => 5, :name => "awexome" }
+  return json @hashy if params[:type] == 'json'
+  haml :test if params[:type] == 'html'
+end
+
+def json (stuff)
+  content_type :json
+  stuff.to_json
 end
